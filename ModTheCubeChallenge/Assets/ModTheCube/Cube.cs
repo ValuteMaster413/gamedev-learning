fa70cb983dae4077d25142a8627dedf398bf82a3
ValuteMaster413 <134705162+ValuteMaster413@users.noreply.github.com>
2026-05-24 16:34:41 +0300

    ModTheCubeChallenge

---
+++ ModTheCubeChallenge/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 4.0f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0f, 128.0f, 128.0f, 1.0f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
